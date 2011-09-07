1817bf2b891de07cccda8a842d6cefba858f2d52
geric001 <geric001@e824114e-66b8-4a2a-92bd-c737042b8df8>
2011-09-07 17:44:12 +0000

    Updated copyright information based on recent changes.

--- branches/opencflite-476/CFBase.c
+++ branches/opencflite-476/CFBase.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2008-2009 Brent Fulgham <bfulgham@gmail.org>.  All rights reserved.
- * Copyright (c) 2009 Grant Erickson <gerickson@nuovations.com>. All rights reserved.
+ * Copyright (c) 2009-2011 Grant Erickson <gerickson@nuovations.com>. All rights reserved.
  *
  * This source code is a modified version of the CoreFoundation sources released by Apple Inc. under
  * the terms of the APSL version 2.0 (see below).

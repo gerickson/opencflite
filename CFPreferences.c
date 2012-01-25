e845514bc511674f75726978b95701d4de6dd686
Brent Fulgham <bfulgham@gmail.com>
2012-01-25 22:29:07 +0000

    Upgrade to CF-635.15

--- CFPreferences.c
+++ CFPreferences.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2011 Brent Fulgham <bfulgham@gmail.org>.  All rights reserved.
+ * Copyright (c) 2008-2012 Brent Fulgham <bfulgham@gmail.org>.  All rights reserved.
  *
  * This source code is a modified version of the CoreFoundation sources released by Apple Inc. under
  * the terms of the APSL version 2.0 (see below).
@@ -44,7 +44,7 @@
 #include <CoreFoundation/CFPropertyList.h>
 #include <CoreFoundation/CFBundle.h>
 #include <CoreFoundation/CFNumber.h>
-#include "CFPriv.h"
+#include <CoreFoundation/CFPriv.h>
 #include <CoreFoundation/CoreFoundation_Prefix.h>
 #include "CFInternal.h"
 #include <sys/stat.h>
@@ -183,8 +183,7 @@ __private_extern__ CFStringRef _CFPreferencesGetByHostIdentifierString(void) {
 static unsigned long __CFSafeLaunchLevel = 0;
 
 #if DEPLOYMENT_TARGET_WINDOWS
-#include <shfolder.h>
-
+#include <shlobj.h>
 #endif
 
 static CFURLRef _preferencesDirectoryForUserHostSafetyLevel(CFStringRef userName, CFStringRef hostName, unsigned long safeLevel) {
